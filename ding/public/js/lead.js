3195abadda60616cef6a676c8701365f47076619
suparikoli <manoj311093@gmail.com>
2024-04-30 21:51:50 +0530

    Lead - Converted- No editing
    Lead has been converted. Editing is disabled

--- ding/public/js/lead.js
+++ ding/public/js/lead.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Lead', {
-    refresh: function(frm) {
+    refresh: function (frm) {
         // Add custom button to create CallLog document
-        frm.add_custom_button(__('CallLog'), function() {
+        frm.add_custom_button(__('CallLog'), function () {
             // Create the Ding Call Logs document
             createDingCallLogs(frm.doc.name, frm.doc.mobile_no, frm.doc.phone);
         });
@@ -21,7 +21,7 @@ frappe.ui.form.on('Lead', {
 
         // Check if mobile_number is present in the lead doctype
         if (frm.doc.mobile_no) {
-            frm.add_custom_button(__('Ding Mobile'), function() {
+            frm.add_custom_button(__('Ding Mobile'), function () {
                 // Play the sound
                 playNotificationSound();
                 // Trigger the call without opening a new tab
@@ -31,7 +31,7 @@ frappe.ui.form.on('Lead', {
 
         // Check if phone is present in the doctype
         if (frm.doc.phone) {
-            frm.add_custom_button(__('Ding Phone'), function() {
+            frm.add_custom_button(__('Ding Phone'), function () {
                 // Play the sound
                 playNotificationSound();
                 // Trigger the call without opening a new tab
@@ -46,17 +46,17 @@ frappe.ui.form.on('Lead', {
         }
 
         // Add custom button to create Lead Meet document
-        frm.add_custom_button(__('Meet'), function() {
+        frm.add_custom_button(__('Meet'), function () {
             frappe.new_doc('Lead Meet', {
                 lead: frm.doc.name
             });
         });
 
         // Add custom button to log location
-        frm.add_custom_button(__('Add Location'), function() {
-            frappe.confirm(__('Do you want to log your current location?'), function() {
+        frm.add_custom_button(__('Add Location'), function () {
+            frappe.confirm(__('Do you want to log your current location?'), function () {
                 // Get user's current location and add it to custom_lead_geolocation
-                navigator.geolocation.getCurrentPosition(function(position) {
+                navigator.geolocation.getCurrentPosition(function (position) {
                     var latitude = position.coords.latitude;
                     var longitude = position.coords.longitude;
                     var geolocation = latitude + ',' + longitude;
@@ -65,5 +65,26 @@ frappe.ui.form.on('Lead', {
                 });
             });
         });
+
+        // Check if status is 'Converted'
+        if (frm.doc.status === 'Converted') {
+            // Disable all fields in the form
+            frm.fields.forEach(function (field) {
+                field.df.read_only = 1;
+                field.refresh();
+            });
+            // Hide the 'Save' button
+            frm.disable_save();
+            // Inform the user
+            frappe.msgprint('Lead has been converted. Editing is disabled by Ding.');
+        } else {
+            // Enable all fields in the form if status is not 'Converted'
+            frm.fields.forEach(function (field) {
+                field.df.read_only = 0;
+                field.refresh();
+            });
+            // Show the 'Save' button
+            frm.enable_save();
+        }
     }
 });
